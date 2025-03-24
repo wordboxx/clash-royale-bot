dd5000a47d038420cd77f1cba33cc9a965e31051
king david <yabokonton@gmail.com>
2025-03-24 03:21:47 -0500

    add: implement GetCardNames function to scrape card names from the card details page

---
+++ utils/cardStats/scrapeCardList.go
@@ -0,0 +1,78 @@
+package cardStats
+
+// IMPORTS
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+// GLOBAL VARIABLES
+var link string = "https://www.deckshop.pro/card/list"
+
+// FUNCTIONS
+func GetCardNames() map[string]interface{} {
+	/*
+	* This function finds the links to the card details page
+	* and extracts the card name from the URL.
+	* It returns a list of card names.
+	 */
+
+	// VARIABLES
+	cardNames := make(map[string]interface{})
+
+	// FUNCTIONS
+	// INSTANTIATE DEFAULT COLLECTOR
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"),
+		colly.AllowURLRevisit(),
+	)
+
+	// BEFORE, AFTER, ON ERROR FUNCTIONS
+	// --- Before making a request
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting:", r.URL)
+	})
+
+	// --- If an error occurs
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	// --- After making a request
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished scraping card names")
+	})
+
+	// SCRAPING FUNCTIONS
+	// --- Find all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// Extract the link
+		href := e.Attr("href")
+
+		// If the link is not empty
+		if href != "" {
+
+			// Isolate links that start with "/card/detail/",
+			// because they contain the card name at the end
+			if strings.HasPrefix(href, "/card/detail/") {
+
+				// Extract card name from URL
+				// Example: /card/detail/ice-spirit = ice-spirit
+				cardName := strings.TrimPrefix(href, "/card/detail/")
+
+				// Get card's stats
+				cardNames[cardName] = GetCardInfo(cardName)
+
+				// Sleep for 3 seconds to avoid getting blocked
+				time.Sleep(3 * time.Second)
+			}
+		}
+	})
+
+	// GOES TO LINK TO START SCRAPING
+	c.Visit(link)
+	return cardNames
+}
