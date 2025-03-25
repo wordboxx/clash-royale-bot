3d3402c5a4a3557e733b396198d9361f97616e41
king david <yabokonton@gmail.com>
2025-03-25 03:06:09 -0500

    feat: add ScrapeAllCards function to scrape card names from external source

---
+++ utils/cardStats/scrapeAllCards.go
@@ -0,0 +1,71 @@
+package cardStats
+
+// IMPORTS
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+// GLOBAL VARIABLES
+var link string = "https://www.deckshop.pro/card/list"
+
+// FUNCTIONS
+func ScrapeAllCards() map[string]interface{} {
+	/*
+	* This function finds the links to the card details page
+	* and extracts the card name from the URL.
+	* It returns a list of card names.
+	 */
+
+	// VARIABLES
+	cardNames := make(map[string]interface{})
+
+	// FUNCTIONS
+	// INSTANTIATE DEFAULT COLLECTOR
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"),
+		colly.AllowURLRevisit(),
+	)
+
+	// BEFORE, AFTER, ON ERROR FUNCTIONS
+	// --- Before making a request
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting:", r.URL)
+	})
+
+	// --- If an error occurs
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	// --- After making a request
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished scraping card names")
+	})
+
+	// SCRAPING FUNCTIONS
+	// --- Loops through all cards
+	var cardList string = "body > main > article > div:nth-child(2) > section:nth-child(2)"
+	c.OnHTML(cardList+" a[href]", func(e *colly.HTMLElement) {
+		// Select link
+		href := e.Attr("href")
+
+		// Isolate card name from end of link
+		if strings.HasPrefix(href, "/card/detail/") {
+			cardName := strings.TrimPrefix(href, "/card/detail/")
+
+			// Extract card's stats
+			cardNames[cardName] = GetCardInfo(cardName)
+
+			// Sleep for 1 second to avoid getting blocked
+			time.Sleep(1 * time.Second)
+		}
+	})
+
+	// GOES TO LINK TO START SCRAPING
+	c.Visit(link)
+	return cardNames
+}
