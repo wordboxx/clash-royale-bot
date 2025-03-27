11815df3677b8998e8fce112ed4b585948c4e46b
king david <yabokonton@gmail.com>
2025-03-27 17:58:20 -0500

    feat: initialize cardStats collector and retrieve card info in main function

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	cardStats "clash-royale-bot/utils/cardStats"
+)
+
 func main() {
+
+	c := cardStats.NewCollector()
+
+	// var urlPrefix string = "https://www.deckshop.pro"
+	// var cardUrl string = urlPrefix + "/card/detail/" + "barbarians"
+
+	cardStats.GetCardInfo("ice-spirit", c)
 }
