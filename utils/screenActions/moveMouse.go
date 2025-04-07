d4b516ddedc7f409e57127725ff62985aeb34c75
wordboxx <yabokonton@gmail.com>
2025-04-07 03:47:41 -0500

    feat: update moveMouse.go to change package name to main and enable screen size retrieval

--- utils/screenActions/moveMouse.go
+++ utils/screenActions/moveMouse.go
@@ -1,8 +1,14 @@
-package screenActions
+// Testing, change package name and stuff later
+package main
 
 // IMPORTS
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	"github.com/go-vgo/robotgo"
-// )
+	"github.com/go-vgo/robotgo"
+)
+
+func main() {
+	sx, sy := robotgo.GetScreenSize()
+	fmt.Println(sx, sy)
+}
