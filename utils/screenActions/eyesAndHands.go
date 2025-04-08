4bd47052808558069c7c7eaedb27da18fd1c4f39
wordboxx <yabokonton@gmail.com>
2025-04-08 03:19:40 -0500

    Add new screenshot 'thingToFind.png' for screen actions utility

--- utils/screenActions/eyesAndHands.go
+++ utils/screenActions/eyesAndHands.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/go-vgo/robotgo"
 	"github.com/vcaesar/bitmap"
 )
 
-func main() {
-	// Variables
-	var screenShotDir string = "utils/screenActions/screenShots/"
+var screenShotDir string = "utils/screenActions/screenShots/"
+
+func directoryInit() {
+	// Screenshot directory handling
+	if _, err := os.Stat(screenShotDir); os.IsNotExist(err) {
+		err := os.MkdirAll(screenShotDir, os.ModePerm)
+		if err != nil {
+			fmt.Println("Error creating directory:", err)
+			return
+		}
+		fmt.Println("Directory created:", screenShotDir)
+	}
+}
+
+func findObjectOnScreen() {
+	// TODO: Gonna have to use OpenCV for this
 	sx, sy := robotgo.GetScreenSize()
 
 	// Capture the screen
@@ -22,5 +36,29 @@ func main() {
 
 	// Save the raw bitmap as PNG
 	bitmap.Save(bit, screenShotDir+"test.png")
-	fmt.Println("Bitmap saved as test.png in ", screenShotDir)
+
+	// Example: Find an image on the screen
+	imagePath := "/home/kingdavid/Documents/clash-royale-bot/utils/screenActions/screenShots/thingToFind.png"
+	bitToFind := bitmap.Open(imagePath)
+	if bitToFind == nil {
+		fmt.Println("Failed to load image:", imagePath)
+		return
+	}
+	defer robotgo.FreeBitmap(bitToFind)
+
+	x, y := bitmap.Find(bitToFind)
+	if x != -1 && y != -1 {
+		robotgo.Move(x, y)
+	} else {
+		fmt.Println("Image not found on the screen.")
+	}
+}
+
+func main() {
+	directoryInit()
+
+	for i := 0; i < 10; i++ {
+		findObjectOnScreen()
+	}
+	fmt.Println("done")
 }
