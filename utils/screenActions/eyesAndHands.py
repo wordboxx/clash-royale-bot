1fa964476cf5a8a7619eedca44348252dd7702f5
wordboxx <yabokonton@gmail.com>
2025-04-13 18:27:22 -0500

    refactor: streamline image searching in eyesAndHands.py by introducing initial region definition and enhancing region adjustment logic

--- utils/screenActions/eyesAndHands.py
+++ utils/screenActions/eyesAndHands.py
@@ -1,34 +1,43 @@
 import pyautogui
 import os
 
-def find_image(image_path, region):
-    try:
-        # Search within the specified region
-        location = pyautogui.locateOnScreen(image_path, confidence=0.8, region=region)
-        if location is not None:
-            print("Image found")
-            x, y = pyautogui.center(location)
-            pyautogui.moveTo(x, y)
-    except Exception as e:
-        print(f"Error in find_image: {e}")
-        find_image(image_path, get_full_screen_region())
-
-
-def get_full_screen_region():
+def define_initial_region():
     screenX, screenY = pyautogui.size()
     return (0, 0, screenX, screenY)
 
-def adjust_search_region(location, screenX, screenY, margin=50):
-    # Expand the region slightly around the last known location
-    # (Speeds up the search by reducing the area)
+def find_image(image_path, region=None):
+    try:
+        # If no region is provided, use the full screen
+        if region is None:
+            region = define_initial_region()
+        
+        location = pyautogui.locateOnScreen(image_path, confidence=0.8, region=region)
+        if location is not None:
+            x, y = pyautogui.center(location)
+            pyautogui.moveTo(x, y)
+            return location
+    except Exception as e:
+        print(f"Error in find_image: {e}")
+        return None
+
+def adjust_search_region(location, margin=50):
     left, top, width, height = location
-    new_left = max(0, left - margin)
-    new_top = max(0, top - margin)
-    new_width = min(screenX, left + width + margin) - new_left
-    new_height = min(screenY, top + height + margin) - new_top
-    return (new_left, new_top, new_width, new_height)
+    search_region = (
+        max(0, left - margin),
+        max(0, top - margin),
+        width + (2 * margin),
+        height + (2 * margin)
+    )
+    return search_region
 
 if __name__ == "__main__":
-    image_path = os.path.join(os.path.dirname(__file__), "imagesToFind", "test.png")
+    image_filepath = os.path.join(os.path.dirname(__file__), "imagesToFind", "test.png")
+    search_region = None
     for i in range(10):
-        find_image(image_path, get_full_screen_region())
+        location = find_image(image_filepath, region=search_region)
+        if location is not None:
+            search_region = adjust_search_region(location)
+            print(search_region)
+        else:
+            print("No location found")
+            location = find_image(image_filepath, region=define_initial_region())
