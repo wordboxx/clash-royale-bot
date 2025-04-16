6250afcc0e6bf6f4062e3de5c056e18557e5584b
wordboxx <yabokonton@gmail.com>
2025-04-16 00:58:23 -0500

    test push

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	cardStats "clash-royale-bot/utils/cardStats"
-
-	"github.com/gocolly/colly"
 )
 
 func main() {
-	c := colly.NewCollector()
-	cardStats.GetCardInfo("witch", c)
+	cardStats.ScrapeAllCards()
 }
